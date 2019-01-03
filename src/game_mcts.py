e3c75c938463068ac22122ca8ce82de2e9a2eb3e
Tong-ZHAO <zhaotong.patricia@gmail.com>
2019-01-03 14:44:49 +0100

    implement MCTS

---
+++ src/game_mcts.py
@@ -0,0 +1,215 @@
+import copy
+import numpy as np
+
+
+FINISH, NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST  = -1, 0, 1, 2, 3
+# List of moves
+Moves = [NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST]
+# Move steps
+coor_matrix = [[-1, -1], # NorthWest
+               [-1,  1], # NorthEast
+               [ 1, -1], # SouthWest
+               [ 1,  1]] # SouthEast
+
+
+class Map:
+
+    def __init__(self, init_map = None):
+        self.map = np.zeros((8, 8)) if map is None else init_map
+
+    def __getitem(self, x, y):
+        return self.map(x, y)
+
+    def __setitem__(self, x, y, value):
+        self.map(x, y) = value
+
+    def __repr__(self):
+        return 'Map\n' + np.array2string(self.map
+
+    def num_pieces(self, white):
+        if white:
+            return (self.map > 0).sum()
+        else:
+            return (self.map < 0).sum()
+
+    def get_player_pieces(self, white):
+        if white:
+            return np.vstack(np.where(self.map > 0)).T
+        else:
+            return np.vstack(np.where(self.map < 0)).T
+
+    @property
+    def array(self):
+        return self.map
+
+
+class Player:
+
+    def __init__(self, name, map, white = False):
+
+        self.name = name
+        self.white = white
+        self.mark = 1 if white else -1
+        self.init_pieces(map)
+
+    def init_pieces(self, map):
+
+        rows = [5, 6, 7] if self.white else [0, 1 ,2]
+
+        for row in rows:
+            cols = [0, 2, 4, 6] if row % 2 == 1 else [1, 3, 5, 7]
+            for col in cols:
+                map.set_piece(row, col, self.mark)
+
+
+class GameState:
+
+    def __init__(self, my_map, player1, player2, pieces = None):
+
+        self.my_map = my_map
+        self.player = player1
+        self.opponent = player2
+
+        if pieces is not None:
+            self.pieces = pieces
+            self.flag_eat = True
+        else:
+            self.pieces = self.my_map.get_player_pieces(self.player.white)
+            self.flag_eat = False
+
+
+def init_game(name_p1 = "white", name_p2 = "black"):
+
+    my_map = Map()
+    player1 = Player(name_p1, my_map, True)
+    player2 = Player(name_p2, my_map, False)
+
+    return GameState(my_map, player1, player2)
+
+
+def move_piece(old_state, curr_x, curr_y, move):
+
+    step_x, step_y = coor_matrix[move[0]]
+    new_x, new_y = None, None
+    my_map = copy.deepcopy(old_state.my_map)
+
+    if move[1] == True:
+        new_x, new_y = curr_x + 2 * step_x, curr_y + 2 * step_y
+        my_map(curr_x + step_x, curr_x + step_y) = 0
+    else:
+        new_x, new_y = curr_x + step_x, curr_y + step_y
+            
+    my_map(new_x, new_y) = my_map(curr_x, curr_y)
+    my_map(curr_x, curr_y) = 0
+
+    val, flag = check_king(my_map, new_x, new_y)
+    my_map(new_x, new_y) = val
+
+    if move[2] or flag:
+        return GameState(my_map, old_state.opponent, old_state.player)
+    else:
+        return GameState(my_map, old_state.player, old_state.opponent, [[new_x, new_y]])
+
+
+def check_king(my_map, x, y):
+
+    mark = my_map(x, y)
+
+    if x == 0 and mark == 1:
+        return 2, True
+    elif x == 7 and mark == -1:
+        return -2, True
+    else:
+        return mark, False
+
+
+def game_over(map)
+
+    num_white = map.num_pieces(True)
+    num_black = map.num_pieces(False)
+
+    # One player has no piece
+    if num_white * num_black == 0:
+        return 1 if num_black == 0 else -1
+
+    # Check White
+    pieces_white = map.get_player_pieces(True)
+    flag_white, flag_black = False, False
+
+    for piece in pieces_white:
+        pathes = find_possible_pathes(map, 1, piece[0], piece[1])
+        if len(pathes) > 0:
+            flag_white = True
+            break
+
+    # Check Black
+    pieces_black = map.get_player_pieces(False)
+
+    for piece in pieces_black:
+        pathes = find_possible_pathes(map, -1, piece[0], piece[1])
+        if len(pathes) > 0:
+            flag_black = True
+            break
+
+    if flag_black and flag_white:
+        return 0
+    elif flag_white: # white wins
+        return 1
+    else:            # black wins
+        return -1
+
+
+def find_possible_pathes(map, mark, curr_x, curr_y):
+
+    piece = map(curr_x, curr_y) 
+    assert(mark * piece > 0), "Illegal Position"
+
+    moves = []
+    pathes, flags_eat, flags_inv = [], [], []
+    if abs(piece) == 2:
+        moves = Moves
+    elif piece == -1:
+        moves = [SOUTHWEST, SOUTHEAST]
+    else:
+        moves = [NORTHWEST, NORTHEAST]
+
+    for move in Move:
+        path = is_possible_path(map, mark, curr_x, curr_y, move)
+        if path[0] != FINISH:
+            pathes.append(path)
+
+    return pathes
+
+
+def is_possible_path(map, mark, curr_x, curr_y, move, eat):
+    """
+    Returns:
+        move     : the move
+        flag_eat : whether we eat a piece in the move
+        flag_inv : whether we change the player
+    """
+
+    mark = player.mark
+    step_x, step_y = coor_matrix[move]
+    new_x , new_y = curr_x + step_x, curr_y + step_y  
+
+    # Handle Cases:
+    #    1. Already reach the border
+    #    2. The destination is taken by its teammate
+    if (new_x < 0) or (new_y < 0) or (new_x > 7) or (new_y > 7) or (map(new_x, new_y) * mark > 0):
+        return [FINISH, False, True]
+    # Handle Case: Available 1 step
+    elif self.map(new_x, new_y) == 0:
+        if eat:
+            return [FINISH, False, True]
+        else:
+            return [move, False, True]
+    # Eat opponent's piece
+    else:
+        jump_x , jump_y = new_x + step_x, new_y + step_y
+        # Handle Case: No available place
+        if (jump_x < 0) or (jump_y < 0) or (jump_x > 7) or (jump_y > 7) or map(jump_x, jump_y) != 0:
+            return [FINISH, False, True]
+        # Jump, update the map, and find following possible move
+        else:
+            return [move, True, False]
---
+++ src/mcts.py
@@ -0,0 +1,123 @@
+import numpy as np
+import game_mcts
+
+class StateNode:
+
+    def __init__(self, parent, game_state):
+        self.parent = parent
+        self.actions = []
+        self.state = game_state
+        self.player = state.player
+        self.gameover = game_mcts.game_over(self.state.map)
+
+    def is_leaf(self):
+        return true if len(actions) == 0 else false
+
+    def init_children(self, prior):
+
+        assert(len(self.action) == 0), "The actions have been initialized!"
+
+        # find all pieces of the current player
+        pieces = self.state.pieces
+        # find possible moves for the player
+        for i in range(len(pieces)):
+            x, y = pieces[i, 0], pieces[i, 1]
+            moves = game_mcts.find_possible_pathes(self.state.map, self.state.player.mark, x, y)
+
+            # add action
+            for move in moves:
+                out_state = game_mcts.move_piece(self.state.map, x, y, move)
+                action = ActionNode(self, [x, y], move, prior[x, y, move[0]))
+                out_node = StateNode(action, out_state)
+                action.set_child(out_node)
+                self.actions.append(action)
+                
+
+    def get_children(self):
+
+        return [self.actions[i].out_node for i in len(actions)]
+
+    def best_children(self):
+
+        ind = np.argmax([action.stats["N"] for action in self.actions])
+
+        return self.actions[ind]
+
+    def get_policy(self):
+
+        policy = np.zeros((8, 8, 4))
+
+        for action in self.actions:
+            policy[action.x, action.y, action.move[0]] = action.stats["N"]
+
+        return policy / policy.sum()
+
+    def get_value(self):
+
+        return self.parent.stats["Q"]
+
+
+
+
+class ActionNode:
+
+    def __init__(self, in_node, coord, action, prior):
+        self.player = in_node.player
+        self.in_node = in_node
+        self.x, self.y = coord
+        self.action = action
+        self.stats = {'N': 0,     # the number of times the action has been taken
+                      'W': 0,     # the total value of the next state
+                      'Q': 0,     # the mean value of the next state
+                      'P': prior  # the prior proba of selecting the action
+                     }
+        self.out_node = None
+
+    def set_child(self, child):
+        self.out_node = child
+
+
+class MCTS:
+
+    def __init__(self, root, cpuct):
+
+        self.root = root
+        self.num_node = 1
+        self.cpuct = cpuct
+
+    def move_to_leaf(self, net, max_iter):
+
+        curr_node = self.root
+        actions = []
+
+        while not curr_node.is_leaf():
+
+            if len(action) > max_iter or curr_node.gameover != 0:
+                break
+
+            max_val, max_action = -np.inf, None
+            N_sum = sum([curr_node.actions[i].stats['N'] for i in range(len(curr_node.actions))])
+
+            for action in curr_node.actions:
+
+                N, W, Q, P = action.stats
+                U = Q + self.cpuct * P * np.sqrt(N_sum) / (1 + N)
+
+                if action.stats['Q'] + U > max_val:
+                    max_val = action.stats['Q'] + U
+                    max_action = action
+
+            actions.append(max_action)
+            curr_node = max_action.out_node
+
+        prior, value = net(curr_node.map)
+
+        curr_node.init_children(prior)
+        self.num_node += len(curr_node.actions)
+
+        # update value for all passed actions
+        for action in actions:
+            action.stats['N'] += 1
+            action.stats['W'] += value
+            action.stats['Q'] = action.stats['W'] / action.stats['N']
+        
